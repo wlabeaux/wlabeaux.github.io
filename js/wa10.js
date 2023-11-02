592aa41ad4231bb3e3f725410d3e55cbf81d6a7d
wlabeaux <wila1321@colorado.edu>
2023-11-02 11:56:59 -0600

    Update wa10.js

--- js/wa10.js
+++ js/wa10.js
@@ -28,7 +28,7 @@ function result() {
 
   if(customName.value !== '') {
     const name = customName.value;
-    newStory.replace("Bob", name);
+    newStory = newStory.replace("Bob", name);
   }
 
   if(document.getElementById("uk").checked) {
